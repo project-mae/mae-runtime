a4f1177d7461718da922863f0328d201feef699f
Mukul Agarwal <agarmukul23@gmail.com>
2021-08-06 23:45:28 -0500

    fix register tests, rename class to MaeABITests

--- Tests/MaeABITests/MaeABITests.swift
+++ Tests/MaeABITests/MaeABITests.swift
@@ -1,22 +1,24 @@
     import XCTest
     @testable import MaeABI
 
-final class MaeRegisterTests: XCTestCase {
-    func registerIndexTests() {
+final class MaeABITests: XCTestCase {
+    func testRegisterIndex() {
         let expected : [Register : UInt8] = [
             .A : 0,
             .B : 1,
             .C : 2,
-            .D : 4
+            .D : 3
         ]
         for (reg, expectedIndex) in expected {
             XCTAssertEqual(reg.id, expectedIndex)
         }
     }
-    func instructionParsingIsReversibleTests() throws {
-        for i in UInt8(0)...UInt8(255) {
-            XCTAssertEqual(try XCTUnwrap(Instruction(rawValue: i)).rawValue, i)
+    func testInstructionParsingIsReversible() throws {
+        for i in UInt8.min...UInt8.max /* 0...255*/ {
+            if let instruction = Instruction(rawValue: i) {
+                let index = try XCTUnwrap(instructionLookupTable.firstIndex(of: instruction))
+                XCTAssertEqual(index, Int(i))
+            }
         }
     }
 }
-
