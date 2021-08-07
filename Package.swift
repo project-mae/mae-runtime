e1025ea2ffda88913ea28d86b817677bc10a0f9b
Mukul Agarwal <agarmukul23@gmail.com>
2021-08-07 13:45:27 -0500

    format Package.swift

--- Package.swift
+++ Package.swift
@@ -4,37 +4,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "MaeRuntime",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "MaeABI", targets: ["MaeABI"]),
-        .library(name: "MaeAssembler", targets: ["MaeAssembler"]),
-        .executable(name: "masm", targets: ["MaeAssembler"])
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-collections",
-            .exact("0.0.3")
-            )
-    ],
-    targets: [
-        .target(
-            name: "MaeABI",
-            dependencies: [
-                .product(name: "Collections", package: "swift-collections")
-            ]),
-        .target(
-            name: "MaeAssembler",
-            dependencies: [
-                .product(name: "Collections", package: "swift-collections"),
-                "MaeABI"
-            ]
-        ),
-        .testTarget(
-            name: "MaeABITests",
-            dependencies: ["MaeABI"]),
-        .testTarget(
-            name: "MaeAssemblerTests",
-            dependencies: ["MaeAssembler"]),
-    ]
+  name: "MaeRuntime",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(name: "MaeABI", targets: ["MaeABI"]),
+    .library(name: "MaeAssembler", targets: ["MaeAssembler"]),
+    .executable(name: "masm", targets: ["MaeAssembler"]),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-collections",
+      .exact("0.0.3")
+    )
+  ],
+  targets: [
+    .target(
+      name: "MaeABI",
+      dependencies: [
+        .product(name: "Collections", package: "swift-collections")
+      ]),
+    .target(
+      name: "MaeAssembler",
+      dependencies: [
+        .product(name: "Collections", package: "swift-collections"),
+        "MaeABI",
+      ]
+    ),
+    .testTarget(
+      name: "MaeABITests",
+      dependencies: ["MaeABI"]),
+    .testTarget(
+      name: "MaeAssemblerTests",
+      dependencies: ["MaeAssembler"]),
+  ]
 )
