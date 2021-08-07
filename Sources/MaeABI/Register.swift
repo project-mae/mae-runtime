09ee6c7e92e7a5e2560f4b5b0ecd70a5ebfa198a
Mukul Agarwal <agarmukul23@gmail.com>
2021-08-07 13:46:32 -0500

    format MaeABI/Register.swift

--- Sources/MaeABI/Register.swift
+++ Sources/MaeABI/Register.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Mukul Agarwal on 6/28/21.
 //
@@ -8,16 +8,16 @@
 import Collections
 
 public enum Register {
-    case A, B, C, D
+  case A, B, C, D
 }
 
 extension Register {
-    private static var registers : OrderedSet<Self> = [.A, .B, .C, .D]
-    private static var publiclyAvailable : Set<Self> = [.A, .B, .C, .D]
-    public var availableToUser : Bool {
-        return Register.publiclyAvailable.contains(self)
-    }
-    public var id : UInt8 {
-        return UInt8(exactly: Register.registers.firstIndex(of: self)!)!
-    }
+  private static var registers: OrderedSet<Self> = [.A, .B, .C, .D]
+  private static var publiclyAvailable: Set<Self> = [.A, .B, .C, .D]
+  public var availableToUser: Bool {
+    return Register.publiclyAvailable.contains(self)
+  }
+  public var id: UInt8 {
+    return UInt8(exactly: Register.registers.firstIndex(of: self)!)!
+  }
 }
