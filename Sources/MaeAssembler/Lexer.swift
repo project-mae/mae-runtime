a00c0678e9b3d4dcb5338a0541d54bf8267a1229
Mukul Agarwal <agarmukul23@gmail.com>
2021-08-07 16:00:57 -0500

    add basic delimiter token lexing and tests

---
+++ Sources/MaeAssembler/Lexer.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//
+//
+//  Created by Mukul Agarwal on 8/7/21.
+//
+
+import Foundation
+
+extension Character {
+  public init(_ byte: UInt8) {
+    self.init(UnicodeScalar(byte))
+  }
+}
+
+public class Lexer: IteratorProtocol {
+  fileprivate(set) var input: Data
+  private let delimiters: [Character: Token] = [
+    ",": .Comma,
+    ":": .Colon,
+    ".": .Period,
+    "(": .LeftParen,
+    ")": .RightParen,
+    "@": .AtSymbol,
+    ";": .Semicolon,
+  ]
+  fileprivate var index: Int = -1
+  init(_ input: Data) {
+    self.input = input
+  }
+  private func next_byte() -> UInt8? {
+    index += 1
+    return current_byte()
+  }
+  private func current_byte() -> UInt8? {
+    guard index < input.count && 0 <= index else { return nil }
+    return input[index]
+  }
+  private func peek_next_byte() -> UInt8? {
+    guard (index + 1) < input.count && -1 <= index else { return nil }
+    return input[index + 1]
+  }
+  public typealias Element = Token
+  public func next() -> Token? {
+    while let currentByte = next_byte() {
+      if let delimiter = delimiters[Character(currentByte)] {
+        return delimiter
+      }
+    }
+    return nil
+  }
+}
---
+++ Sources/MaeAssembler/Token.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//
+//
+//  Created by Mukul Agarwal on 8/7/21.
+//
+
+public enum Token: Equatable {
+  case Identifier(String)
+  case Literal(UInt8)
+  case Comma
+  case Colon
+  case Period
+  case Section
+  case LeftParen
+  case RightParen
+  case AtSymbol
+  case Semicolon
+}
