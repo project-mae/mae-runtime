fc65fce72cb99063535ae3db56b014df6256e67b
Mukul Agarwal <agarmukul23@gmail.com>
2021-08-09 11:34:13 -0500

    fix: change XCTExpectFailure to XCTSkip as the XCTExpectFailure api does not yet exist on Linux.

--- Tests/MaeAssemblerTests/MaeAssemblerTests.swift
+++ Tests/MaeAssemblerTests/MaeAssemblerTests.swift
@@ -21,8 +21,8 @@ final class MaeAssemblerTests: XCTestCase {
 			XCTAssertEqual(token, lexer.next())
 		}
 	}
-	func testSingleNumberLexing() {
-		XCTExpectFailure("Not yet implemented.")
+	func testSingleNumberLexing() throws {
+		throw XCTSkip("Not yet implemented.")
 		let inputs = (1...100)
 			.map { _ in UInt8.random(in: 0...255) }
 		for i in inputs {
@@ -30,8 +30,8 @@ final class MaeAssemblerTests: XCTestCase {
 			XCTAssertEqual(lexer.next(), Token.Literal(i))
 		}
 	}
-	func testSingleIdentifierLexing() {
-		XCTExpectFailure("Not yet implemented.")
+	func testSingleIdentifierLexing() throws {
+		throw XCTSkip("Not yet implemented.")
 		let inputs = [
 			"HLT", "ZRG", "CP", "SWP", "ZRM", "LDA", "STA", "ADD", "SUB", "MUL", "DIV",
 			"AND", "OR", "NOT", "XOR",
